a70118928bbb47813a599fbe7d84ccd827ae66f3
melchisedech333 <melchisedech333@gmail.com>
2022-05-02 20:19:58 -0300

    Updates

---
+++ vm/semantics-analisys.cc
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "configuration.h"
+#include "ast-struct.h"
+#include "semantics-analisys.h"
+
+using namespace verbum;
+using namespace std;
+
+void verbum_semantics_analisys::custom_method () 
+{
+    cout << "called()\n";
+}
+
+
---
+++ vm/semantics-analisys.h
@@ -0,0 +1,36 @@
+
+/*
+** Copyright (c) 2022, the Verbum project authors.  Please see the AUTHORS file
+** for details. All rights reserved. Use of this source code is governed by a
+** BSD-style license that can be found in the LICENSE file.
+**
+** In principio erat Verbum et Verbum erat apud Deum et Deus erat Verbum - John 1
+**
+** Realização da análise semântica.
+*/
+
+#ifndef VERBUM_SEMANTICS_ANALISYS
+#define VERBUM_SEMANTICS_ANALISYS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "ast-struct.h"
+
+using namespace std;
+
+namespace verbum {
+    class verbum_semantics_analisys : public verbum_semantics
+    {
+        public:
+            void custom_method ();
+    };
+}
+
+#endif
+
+
