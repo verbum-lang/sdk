d1037c0d06cca2c2816fc9d711b050740d5f6409
melchisedech333 <melchisedech333@gmail.com>
2022-08-19 20:35:12 -0300

    Updates

--- verbum-network/source/verbum-network/help.c
+++ verbum-network/source/verbum-network/help.c
@@ -6,8 +6,14 @@ void show_help (void)
 {
     printf(
         "\n"
-        "Use: verbum-network -c SETTINGS-FILE.ini\n"
-        "Note: The configuration file uses the INI format.\n"
+        "Verbum Programming Language - Verbum Network\n"
+        "\n"
+        "   Use:     verbum-network OPTION PARAM\n"
+        "   Example: verbum-network -c filename.ini\n"
+        "\n"
+        "Options:\n"
+        "   -c CONFIGURATION-FILE   - Specify configuration file.\n"
+        "                             The configuration file uses the INI format.\n"
         "\n"
     );
 
