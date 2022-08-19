d3fd409efadd7997ba0c52bedea9122692a10265
melchisedech333 <melchisedech333@gmail.com>
2022-08-19 19:53:21 -0300

    Updates

--- verbum-network/source/verbum-network/verbum-network.c
+++ verbum-network/source/verbum-network/verbum-network.c
@@ -2,34 +2,48 @@
 #include "global.h"
 #include "help.h"
 
-void initialization (int argc, char *argv[]);
+int param_argc = 0;
+char **param_argv = { NULL };
+
+void initialization (void);
+void configutation_check (void);
+void configutation_read (void);
 
 int main (int argc, char *argv[]) 
 {
-    initialization(argc, argv);
+    param_argc = argc;
+    param_argv = argv;
+
+    initialization();
 
     return 0;
 }
 
-void initialization (int argc, char *argv[]) 
+void initialization (void) 
+{
+    configutation_check();
+    configutation_read();
+}
+
+void configutation_check (void)
 {
     char *configuration_file = NULL;
     int size = 0;
 
-    for (int a=0; a<argc; a++) 
-    {
-        if (strcmp(argv[a], "-c") == 0 && (a+1) < argc) {
-            memory_scopy(argv[a+1], configuration_file);
-        }
-    }
+    for (int a=0; a<param_argc; a++) 
+        if (strcmp(param_argv[a], "-c") == 0 && (a+1) < param_argc) 
+            memory_scopy(param_argv[a+1], configuration_file);
 
     if (!configuration_file)
         show_help();
 
     if (!file_exists(configuration_file))
         say_exit("The file does not exist: %s", configuration_file);
+}
 
-    printf("config file: %s\n", configuration_file);
+void configutation_read (void)
+{
+    
 }
 
 
