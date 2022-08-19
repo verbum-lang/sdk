25162cf800a4cf9f7eb01694c1cb78e7b012655a
melchisedech333 <melchisedech333@gmail.com>
2022-08-19 17:51:24 -0300

    Updates

--- verbum-network/build-scripts/rebuild-all.sh
+++ verbum-network/build-scripts/rebuild-all.sh
@@ -7,11 +7,13 @@
 cd ../../../sdk-binaries
 rm -rf verbum-network verbum-node-mapper verbum-fault-tolerance
 
+
 # ***
 # Build verbum-network
 #
 
 cp ../sdk/verbum-network/source/verbum-network/verbum-network ./
+
 
 # ***
 # Build verbum-node-mapper
@@ -25,5 +27,20 @@ gcc -o verbum-node-mapper verbum-node-mapper.c
 
 # Move to install directory.
 mv verbum-node-mapper ../../../../sdk-binaries
+
+
+# ***
+# Build verbum-fault-tolerance
+#
+
+cd ../verbum-fault-tolerance
+rm -rf *.o
+
+# Compile.
+gcc -o verbum-fault-tolerance verbum-fault-tolerance.c
+
+# Move to install directory.
+mv verbum-fault-tolerance ../../../../sdk-binaries
+
 
 
---
+++ verbum-network/source/verbum-fault-tolerance/verbum-fault-tolerance.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+    printf("Verbum Fault Tolerance\n");
+
+    return 0;
+}
+
+
