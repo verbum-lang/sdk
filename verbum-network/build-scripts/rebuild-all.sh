a7783f1011d781eff1cb247d13fffb4f8f1186fa
melchisedech333 <melchisedech333@gmail.com>
2022-08-20 22:46:44 -0300

    Updates

--- verbum-network/build-scripts/rebuild-all.sh
+++ verbum-network/build-scripts/rebuild-all.sh
@@ -20,6 +20,8 @@ gcc -o application.o -c application.c
 gcc -o file.o -c file.c
 gcc -o ini_file.o -c ini_file.c
 
+LIBRARY_OBJECTS="../library/global.o ../library/application.o ../library/file.o ../library/ini_file.o"
+
 # ***
 # Build verbum-node
 #
@@ -33,10 +35,12 @@ gcc -o help.o -c help.c
 gcc -o configuration.o -c configuration.c
 gcc -o node.o -c node.c
 
-gcc -o verbum-node verbum-node.c global.o help.o configuration.o              \
-    node.o                                                                   \
-    ../library/global.o ../library/application.o ../library/file.o               \
-    ../library/ini_file.o
+gcc -o verbum-node verbum-node.c        \
+                   global.o             \
+                   help.o               \
+                   configuration.o      \
+                   node.o               \
+                   $LIBRARY_OBJECTS
 
 rm -rf *.o
 
@@ -57,10 +61,12 @@ gcc -o help.o -c help.c
 gcc -o configuration.o -c configuration.c
 gcc -o node-mapper.o -c node-mapper.c
 
-gcc -o verbum-node-mapper verbum-node-mapper.c global.o help.o configuration.o      \
-    node-mapper.o                                                                   \
-    ../library/global.o ../library/application.o ../library/file.o               \
-    ../library/ini_file.o
+gcc -o verbum-node-mapper verbum-node-mapper.c  \
+                          global.o              \
+                          help.o                \
+                          configuration.o       \
+                          node-mapper.o         \
+                          $LIBRARY_OBJECTS
 
 rm -rf *.o
 
@@ -79,10 +85,11 @@ rm -rf *.o
 gcc -o global.o -c global.c
 gcc -o help.o -c help.c
 gcc -o configuration.o -c configuration.c
-gcc -o verbum-fault-tolerance verbum-fault-tolerance.c global.o help.o              \
-    configuration.o \
-    ../library/global.o ../library/application.o                  \
-    ../library/file.o ../library/ini_file.o
+gcc -o verbum-fault-tolerance verbum-fault-tolerance.c  \
+                              global.o                  \
+                              help.o                    \
+                              configuration.o           \
+                              $LIBRARY_OBJECTS
 
 rm -rf *.o
 
