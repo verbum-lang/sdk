c4292cee05449e03ef164f06bfda4ea09eed5a2e
melchisedech333 <melchisedech333@gmail.com>
2022-08-20 22:48:23 -0300

    Updates

--- verbum-network/build-scripts/rebuild-all.sh
+++ verbum-network/build-scripts/rebuild-all.sh
@@ -5,7 +5,7 @@
 #
 
 cd ../../../sdk-binaries
-rm -rf verbum-network verbum-node-mapper verbum-fault-tolerance
+rm -rf verbum-node verbum-node-mapper verbum-fault-tolerance
 
 
 # ***
@@ -15,10 +15,10 @@ rm -rf verbum-network verbum-node-mapper verbum-fault-tolerance
 cd ../sdk/verbum-network/source/library
 rm -rf *.o
 
-gcc -o global.o -c global.c
-gcc -o application.o -c application.c
-gcc -o file.o -c file.c
-gcc -o ini_file.o -c ini_file.c
+gcc -o global.o         -c global.c
+gcc -o application.o    -c application.c
+gcc -o file.o           -c file.c
+gcc -o ini_file.o       -c ini_file.c
 
 LIBRARY_OBJECTS="../library/global.o ../library/application.o ../library/file.o ../library/ini_file.o"
 
@@ -30,10 +30,10 @@ cd ../verbum-node
 rm -rf *.o
 
 # Compile.
-gcc -o global.o -c global.c
-gcc -o help.o -c help.c
-gcc -o configuration.o -c configuration.c
-gcc -o node.o -c node.c
+gcc -o global.o         -c global.c
+gcc -o help.o           -c help.c
+gcc -o configuration.o  -c configuration.c
+gcc -o node.o           -c node.c
 
 gcc -o verbum-node verbum-node.c        \
                    global.o             \
@@ -56,10 +56,10 @@ cd ../verbum-node-mapper
 rm -rf *.o
 
 # Compile.
-gcc -o global.o -c global.c
-gcc -o help.o -c help.c
-gcc -o configuration.o -c configuration.c
-gcc -o node-mapper.o -c node-mapper.c
+gcc -o global.o         -c global.c
+gcc -o help.o           -c help.c
+gcc -o configuration.o  -c configuration.c
+gcc -o node-mapper.o    -c node-mapper.c
 
 gcc -o verbum-node-mapper verbum-node-mapper.c  \
                           global.o              \
@@ -85,6 +85,7 @@ rm -rf *.o
 gcc -o global.o -c global.c
 gcc -o help.o -c help.c
 gcc -o configuration.o -c configuration.c
+
 gcc -o verbum-fault-tolerance verbum-fault-tolerance.c  \
                               global.o                  \
                               help.o                    \
