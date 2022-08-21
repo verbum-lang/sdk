83b06a4ab23c19e56e1ac000ce46db1b224dbb9e
melchisedech333 <melchisedech333@gmail.com>
2022-08-21 02:01:54 -0300

    Updates

--- verbum-network/source/verbum-node/node.c
+++ verbum-network/source/verbum-node/node.c
@@ -7,17 +7,9 @@
 void verbum_node (void)
 {
     prepare_node_mapper_fault_tolerance();
+
+    // Continue if all ok!
 }
-
-void check_connection_interface (char *path, int node_mapper_port, int fault_tolerance_port);
-void * prepara_nm_ft_handler (void *param);
-void open_nm_ft_process (char *path);
-
-typedef struct {
-    char *path;
-    int node_mapper_port;
-    int fault_tolerance_port;
-} prepare_param_t;
 
 void prepare_node_mapper_fault_tolerance (void)
 {
@@ -35,17 +27,14 @@ void prepare_node_mapper_fault_tolerance (void)
 
 void * prepara_nm_ft_handler (void *param)
 {
-    char *path = CNULL;
-    int node_mapper_port = 0, fault_tolerance_port = 0;
     prepare_param_t prepare_param = *( (prepare_param_t *) (param) );
+    open_nm_ft_process(prepare_param.path);
 
-    say("param: %s", prepare_param.path);
-
-    // while (1) {
-    //     open_nm_ft_process(path);
-    //     check_connection_interface(path, node_mapper_port, fault_tolerance_port);
-    //     sleep(1);
-    // }
+    while (1) {
+        check_connection_interface(prepare_param.path, 
+            prepare_param.node_mapper_port, prepare_param.fault_tolerance_port);
+        sleep(1);
+    }
 }
 
 void open_nm_ft_process (char *path)
@@ -64,7 +53,6 @@ void check_connection_interface (char *path, int node_mapper_port, int fault_tol
         "Node Mapper", node_mapper_port, "Verbum Node Mapper")) 
     {
         pid_t pid = check_process_running("verbum-node-mapper");
-
         if (pid == -1)
             system_execution("verbum-node-mapper -c \"%s\" &", path);
     }
@@ -79,7 +67,6 @@ void check_connection_interface (char *path, int node_mapper_port, int fault_tol
         "Fault Tolerance", fault_tolerance_port, "Verbum Fault Tolerance"))
     {
         pid_t pid = check_process_running("verbum-fault-tolerance");
-
         if (pid == -1)
             system_execution("verbum-fault-tolerance -c \"%s\" &", path);
     }
--- verbum-network/source/verbum-node/node.h
+++ verbum-network/source/verbum-node/node.h
@@ -4,8 +4,17 @@
 
 #include "global.h"
 
-void verbum_node                         (void);
-void prepare_node_mapper_fault_tolerance (void);
+typedef struct {
+    char *path;
+    int node_mapper_port;
+    int fault_tolerance_port;
+} prepare_param_t;
+
+void verbum_node                            (void);
+void prepare_node_mapper_fault_tolerance    (void);
+void check_connection_interface             (char *path, int node_mapper_port, int fault_tolerance_port);
+void * prepara_nm_ft_handler                (void *param);
+void open_nm_ft_process                     (char *path);
 
 #endif
 
