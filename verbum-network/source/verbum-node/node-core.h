2229f70234c7842189d003f29027ee7fe4bb7cbc
melchisedech333 <melchisedech333@gmail.com>
2022-08-25 23:01:19 -0300

    Updates

--- verbum-network/source/verbum-node/node-core.h
+++ verbum-network/source/verbum-node/node-core.h
@@ -5,9 +5,9 @@
 #include "global.h"
 #include "node-global.h"
 
-void * node_core (void *tparam);
+void * node_core             (void *tparam);
 void add_node_on_node_mapper (node_param_t *param);
-void * ping_node_handler (void *tparam);
+void * ping_node_handler     (void *tparam);
 
 #endif
 
