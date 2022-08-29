4fe6c5640ce75da98a88dbb3aeea89b8b1870dcb
melchisedech333 <melchisedech333@gmail.com>
2022-08-29 20:56:12 -0300

    Updates

--- verbum-network/source/library/datetime.c
+++ verbum-network/source/library/datetime.c
@@ -6,15 +6,39 @@
 
 char * make_datetime (void) 
 {
-    char * date = CNULL;
-    time_t now = time(NULL);
-    struct tm *tms = localtime(&now);
-
+    time_t now      = time(NULL);
+    struct tm *tms  = localtime(&now);
+    char * date     = CNULL;
+    char day[5], mon[5], year[5], hour[5], min[5], sec[5];
+    
     memory_alloc(date, 100);
+    memset(day,  0x0, 5);
+    memset(mon,  0x0, 5);
+    memset(year, 0x0, 5);
+    memset(hour, 0x0, 5);
+    memset(min,  0x0, 5);
+    memset(sec,  0x0, 5);
 
-    sprintf(date, "%d-%d-%d %d-%d-%d", 
-        tms->tm_mday, tms->tm_mon, tms->tm_year,
-        tms->tm_hour, tms->tm_min, tms->tm_sec);
+    sprintf(day,  "%d", tms->tm_mday);
+    sprintf(mon,  "%d", tms->tm_mon);
+    sprintf(year, "%d", (1900 + tms->tm_year));
+    sprintf(hour, "%d", tms->tm_hour);
+    sprintf(min,  "%d", tms->tm_min);
+    sprintf(sec,  "%d", tms->tm_sec);
+
+    if (strlen(day) == 1)
+        sprintf("0%d", tms->tm_mday);
+    if (strlen(mon) == 1)
+        sprintf("0%d", tms->tm_mon);
+    if (strlen(hour) == 1)
+        sprintf("0%d", tms->tm_hour);
+    if (strlen(min) == 1)
+        sprintf("0%d", tms->tm_min);
+    if (strlen(sec) == 1)
+        sprintf("0%d", tms->tm_sec);
+
+    sprintf(date, "%s-%s-%s %s-%s-%s", 
+        day, mon, year, hour, min, sec);
 
     return date;
 }
