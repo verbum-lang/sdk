322c911c47cc1207a88ab26e85f557a972e89add
melchisedech333 <melchisedech333@gmail.com>
2022-08-31 15:36:26 -0300

    Updates

--- verbum-network/source/verbum-node-mapper-manager/application/js/application.js
+++ verbum-network/source/verbum-node-mapper-manager/application/js/application.js
@@ -26,7 +26,7 @@ function get_node_list (hostname, hostport)
 function process_node_list (response) 
 {
     var parts = response.split('\n\n');
-    var nodes = [];
+    var node_items = [];
 
     for (var a=1; a<parts.length; a++) {
         var item  = parts[a].toString().trim();
@@ -64,7 +64,7 @@ function process_node_list (response)
         if (node != -1    && port != -1      &&
             id.length > 0 && date.length > 0  )
         {
-            nodes.push({
+            node_items.push({
                 node: node,
                 id: id,
                 port: port,
@@ -73,12 +73,107 @@ function process_node_list (response)
         }
     }
 
-    update_node_list_information(nodes);
+    update_node_list_information(node_items);
 }
 
-function update_node_list_information (nodes) 
+var nodes = [];
+
+function update_node_list_information (node_items) 
 {
-    
+    var found = false;
+
+    for (var a=0; a<node_items.length; a++) {
+        var item = node_items[a];
+
+        // Search node.
+        found = false;
+
+        for (var b=0; b<nodes.length; b++) {
+            if (nodes[b].id == item.id) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found == false) {
+            nodes = node_items;
+            render_all_nodes();
+            break;
+        }
+    }    
 }
+
+function render_all_nodes ()
+{
+    var html = `
+        <table class="table table-dark table-borderless">
+            <tbody>
+    `;
+
+    for (var a=0; a<nodes.length; a++) {
+        var node = nodes[a];
+
+        html += `
+            <tr>
+                <th>
+                    <table class="table table-dark table-borderless table-results-sec-1">
+                        <tbody>
+                            <tr>
+                                <th>
+                                    <div class="item">
+                                        <div class="sub-1">Node</div>
+                                        <div class="sub-2">`+ node.node +`</div>
+                                    </div>
+                                </th>
+                                
+                                <th>
+                                    <div class="item">
+                                        <div class="sub-1">Identification</div>
+                                        <div class="sub-2">`+ node.id +`</div>
+                                    </div>
+                                </th>
+                                
+                                <th>
+                                    <div class="item">
+                                        <div class="sub-1">Port</div>
+                                        <div class="sub-2">`+ node.port +`</div>
+                                    </div>
+                                </th>
+                                
+                                <th>
+                                    <div class="item">
+                                        <div class="sub-1">Last connection date</div>
+                                        <div class="sub-2">`+ node.last_connection_date +`</div>
+                                    </div>
+                                </th>
+                                
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <table class="table table-dark table-borderless table-results-sec-2">
+                        <tbody>
+                            <tr>
+                                <th>
+                                    Jesus s2
+                                </th>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <hr>
+                </th>
+            </tr>
+        `;
+    }
+
+    html += `
+        </tbody>
+        </table>
+    `;
+
+    $('.area-results').html(html);
+}
+
 
 
