8aed61202f507514665f6c2f170bf8c4e23d2853
melchisedech333 <melchisedech333@gmail.com>
2022-09-10 20:20:09 -0300

    Updates

--- verbum-network/source/verbum-node-mapper/delete-node.c
+++ verbum-network/source/verbum-node-mapper/delete-node.c
@@ -1,9 +1,13 @@
 
 #include "delete-node.h"
 #include "node-control.h"
+#include "connection-control.h"
 
-extern node_control_t *nodes;
-extern pthread_mutex_t mutex_nodes;
+extern node_control_t    *nodes;
+extern pthread_mutex_t    mutex_nodes;
+
+extern pthread_mutex_t    mutex_connections;
+extern node_connection_t *connections;
 
 int delete_node (int sock, char *content)
 {
@@ -20,7 +24,8 @@ int delete_node (int sock, char *content)
     char address           [] = LOCALHOST;
     char *ptr = NULL;
     int bytes = 0, status = 0, counter = 0, core_port = 0;
-    node_control_t *node;
+    node_control_t *node, *nlast;
+    node_connection_t *con, *last;
 
     // Extract node ID.
     ptr = strstr(content, prefix);
@@ -31,25 +36,69 @@ int delete_node (int sock, char *content)
     memset(tmp, 0x0, 1024);
     memcpy(tmp, ptr, strlen(ptr));
 
-    // Search node.
+    // Remove node (struct node control).
     pthread_mutex_lock(&mutex_nodes);
     
     for (node=nodes; node!=NULL; node=node->next) {
-        if (node->status != 1)
+        if (node->status != 1) {
+            nlast = node;
             continue;
+        }
 
         if (strcmp(node->id, tmp) == 0) {
             core_port = node->core_port;
             status = 1;
-            node->status = 0;
+            
+            if (!node->next)
+                nlast->next = NULL;
+            else
+                nlast->next = node->next;
+
+            mem_sfree(node->id);
+            free(node);
             break;
         }
+
+        nlast = node;
     }
 
     pthread_mutex_unlock(&mutex_nodes);
 
-    // Send request to node.
-    if (status) {
+    // Remove node connections.
+    if (status == 1) {
+        pthread_mutex_lock(&mutex_connections);
+        
+        for (con=connections; con!=NULL; con=con->next) {
+            if (con->status != 1 || !con->id || !con->dst_node_id) {
+                last = con;
+                continue;
+            }
+
+            // Remove item.
+            if (strcmp(con->src_node_id, tmp) == 0) {
+                if (!con->next)
+                    last->next = NULL;
+                else 
+                    last->next = con->next;
+
+                mem_sfree(con->id);
+                mem_sfree(con->src_node_id);
+                mem_sfree(con->dst_node_id);
+                mem_sfree(con->dst_nm_id);
+                mem_sfree(con->dst_nm_address);
+                free(con);
+
+                break;
+            }
+
+            last = con;
+        }
+
+        pthread_mutex_unlock(&mutex_connections);
+    }
+
+    // Send request to node auto-close.
+    if (status == 1) {
         while (1) {
             char *response = process_delete_node(address, core_port, tmp);
             if (response) {
