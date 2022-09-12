c8f826bfad3247806a841476848fd358437e69f6
melchisedech333 <melchisedech333@gmail.com>
2022-09-12 00:09:37 -0300

    Updates

--- verbum-network/source/verbum-node/delete-connection.c
+++ verbum-network/source/verbum-node/delete-connection.c
@@ -1,15 +1,19 @@
 
 #include "delete-connection.h"
+#include "node-connection.h"
 
 extern pthread_mutex_t  mutex_gconfig;
 extern node_config_t   *gconfig;
+
+extern pthread_mutex_t    mutex_connections;
+extern node_connection_t *connections;
 
 int delete_connection (int sock, char *content)
 {
     char prefix [] = "delete-verbum-connection:";
     char response_success  [] = VERBUM_DEFAULT_SUCCESS "-IHS-" VERBUM_EOH;
     char response_error    [] = VERBUM_DEFAULT_ERROR   VERBUM_EOH;
-    int bytes = 0;
+    int bytes = 0, status = 0;
 
     char tmp [1024];
     char *ptr = NULL;
@@ -18,6 +22,8 @@ int delete_connection (int sock, char *content)
     char *src_node_id       = NULL;
     char *dst_node_id       = NULL;
     int   connection_type   = -1; // 0 = Input, 1 = Output.
+
+    node_connection_t *connection, *last;
 
     if (!sock || !content)
         goto error;
@@ -67,10 +73,57 @@ int delete_connection (int sock, char *content)
     if (!connection_id || !src_node_id || !dst_node_id || connection_type == -1)
         return 0;
     
-    say("con type: %d", connection_type);
-    say("con id: %s", connection_id);
-    say("src node: %s", src_node_id);
-    say("dst node: %s", dst_node_id);
+    // Check node.
+    pthread_mutex_lock(&mutex_gconfig);
+
+    if (strcmp(gconfig->information.id, src_node_id) == 0)
+        status = 1;
+
+    pthread_mutex_unlock(&mutex_gconfig);
+
+    if (!status)
+        goto error;
+
+    // Search connection, and delete.
+    status = 0;
+    pthread_mutex_lock(&mutex_connections);
+
+    for (connection=connections; connection != NULL; connection=connection->next) {
+        if (!connection->id) {
+            last = connection;
+            continue;
+        } else if (!connection->dst_node_id) {
+            last = connection;
+            continue;
+        }
+
+        if (strcmp(connection->id, connection_id) == 0          &&
+            strcmp(connection->dst_node_id, dst_node_id) == 0   &&
+            connection->type == connection_type                  )
+        {
+            if (!connection->next)
+                last->next = NULL;
+            else 
+                last->next = connection->next;
+
+            mem_sfree(connection->id);
+            mem_sfree(connection->remote_id);
+            mem_sfree(connection->dst_node_id);
+            mem_sfree(connection->dst_nm_id);
+            mem_sfree(connection->dst_nm_address);
+            free(connection);
+
+            status = 1;
+            break;
+        }
+
+        last = connection;
+    }
+
+    pthread_mutex_unlock(&mutex_connections);
+
+    if (!status)
+        goto error;
 
     // Finish.
     success:
