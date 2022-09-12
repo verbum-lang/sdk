9d6be423ab0c89486fac3078078d5ed58f525b09
melchisedech333 <melchisedech333@gmail.com>
2022-09-12 02:45:08 -0300

    Updates

--- verbum-network/source/verbum-node-mapper/timeout-control.h
+++ verbum-network/source/verbum-node-mapper/timeout-control.h
@@ -10,14 +10,14 @@
 #define VERBUM_NODE_SEC_TIMEOUT_ERROR               10
 
 // Auto remove node.
-#define VERBUM_NODE_AUTO_REMOVE
+// #define VERBUM_NODE_AUTO_REMOVE
 #define VERBUM_NODE_SEC_TIMEOUT_AUTO_REMOVE         30
 
 // Update error connection flag.
 #define VERBUM_CONNECTION_SEC_TIMEOUT_ERROR         10
 
 // Auto remove connection.
-#define VERBUM_CONNECTION_AUTO_REMOVE
+// #define VERBUM_CONNECTION_AUTO_REMOVE
 #define VERBUM_CONNECTION_SEC_TIMEOUT_AUTO_REMOVE   30
 
 void *timeout_control (void *tparam);
