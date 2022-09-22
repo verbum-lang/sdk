3cb802887dd6461b54db1719c08dfcda6366dc53
melchisedech333 <melchisedech333@gmail.com>
2022-09-22 17:57:55 -0300

    Updates

--- source/verbum-node-mapper/create-node.c
+++ source/verbum-node-mapper/create-node.c
@@ -17,8 +17,6 @@ int node_mapper_create_node (int sock, char *path)
     char response [] = VERBUM_DEFAULT_SUCCESS VERBUM_EOH;
     int status       = -1;
 
-    // open_application(VERBUM_NODE_APPLICATION);
-    // system_execution_ret(0, "verbum-node -c \"%s\" &", path);
     pthread_create(&tid, NULL, start_verbum_node, NULL);
 
     status = send(sock, response, strlen(response), VERBUM_SEND_FLAGS);
