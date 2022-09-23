eb6e1cf8bee0392d019b81bff4cc59f9d42d6607
melchisedech333 <melchisedech333@gmail.com>
2022-09-23 16:11:09 -0300

    Updates

--- source/library/debug.c
+++ source/library/debug.c
@@ -1,4 +1,70 @@
 
 #include "debug.h"
 
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  // inet_pton
+#include <fcntl.h>      // fcntl
+#include <errno.h>      // errno
+
+/**
+ * Send data to log viewer server.
+ * On success, returns 1. On error, returns 0.
+ */
+
+int verbum_debug_send_data (char *content)
+{
+    struct sockaddr_in address;
+    int sock, flags, status = 0, counter = 0, limit = 10;
+
+    if (!content)
+        return 0;
+
+    address.sin_family      = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port        = htons(VERBUM_DEBUG_SERVER_PORT);
+
+    if (inet_pton(AF_INET, VERBUM_DEBUG_R_ADDRESS, &address.sin_addr) <= 0)
+        say_ret(0, "error set IP address - socket configuration.");
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) 
+        say_ret(0, "creating socket error.");
+    
+    // Enable non-blocking.
+    flags = fcntl(sock, F_GETFL, 0); 
+    if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1)
+        say_ret(0, "error set non-blocking socket.");
+
+    // Connect.
+    if (connect(sock, (struct sockaddr*) &address, sizeof(address)) == -1) {
+        if (errno != EINPROGRESS) 
+            say_ret(0, "connect error");
+    }
+
+    // Disable non blocking.
+    if (fcntl(sock, F_SETFL, flags) == -1) {
+        close(sock);
+        say_ret(0, "error remove non-blocking socket.");
+    }
+
+    // Send data.
+    while (counter <= limit) {
+        if (send(sock, content, strlen(content), MSG_NOSIGNAL) == strlen(content)) {
+            status = 1;
+            break;
+        }
+
+        counter++;
+    }
+
+    if (!status) {
+        close(sock);
+        return 0;
+    }
+
+    close(sock);
+    return 1;
+}
+
 
--- source/library/debug.h
+++ source/library/debug.h
@@ -1,47 +1,90 @@
 
-#ifndef VERBUM_LIBRARY_DEBUG
-#define VERBUM_LIBRARY_DEBUG
+#ifndef VERBUM_DEBUG
+#define VERBUM_DEBUG
+
+#include <stdio.h>  // sprintf
+#include <string.h> // strlen, memset
+#include <unistd.h> // sleep
+#include <time.h>   // time, localtime
+#include <stdlib.h> // malloc, free
+
+/**
+ * Settings.
+ */
+
+#define VERBUM_DEBUG_ENABLED
+#define VERBUM_DEBUG_SERVER_PORT            1337
+#define VERBUM_DEBUG_R_ADDRESS              "127.0.0.1"
+#define VERBUM_DEBUG_CONNECTION_TIMEOUT     1
+
+#define lib_debug_print_internal(DEBUG_FLAG, FMT, ...)                      \
+    do {                                                                    \
+        time_t now     = time(NULL);                                        \
+        struct tm *tms = localtime(&now);                                   \
+        char *buffer   = NULL;                                              \
+        int size       = 0;                                                 \
+        char hour[5], min[5], sec[5];                                       \
+                                                                            \
+        memset(hour, 0x0, 5);                                               \
+        memset(min,  0x0, 5);                                               \
+        memset(sec,  0x0, 5);                                               \
+                                                                            \
+        sprintf(hour, "%d", tms->tm_hour);                                  \
+        sprintf(min,  "%d", tms->tm_min);                                   \
+        sprintf(sec,  "%d", tms->tm_sec);                                   \
+                                                                            \
+        if (strlen(hour) == 1)                                              \
+            sprintf(hour, "0%d", tms->tm_hour);                             \
+        if (strlen(min) == 1)                                               \
+            sprintf(min, "0%d",  tms->tm_min);                              \
+        if (strlen(sec) == 1)                                               \
+            sprintf(sec, "0%d",  tms->tm_sec);                              \
+                                                                            \
+        size   = sizeof(char) * 4096;                                       \
+        buffer = (char *) malloc(size);                                     \
+                                                                            \
+        if (buffer) {                                                       \
+            memset(buffer, 0, size);                                        \
+                                                                            \
+            if (DEBUG_FLAG == 1)                                            \
+                sprintf(buffer, "[%s:%s:%s] -> %s:%d:%s(): " FMT "\n",      \
+                    hour, min, sec,                                         \
+                        __FILE__, __LINE__, __func__, ##__VA_ARGS__ );      \
+            else                                                            \
+                sprintf(buffer, "[%s:%s:%s]: " FMT "\n",                    \
+                    hour, min, sec, ##__VA_ARGS__ );                        \
+                                                                            \
+            verbum_debug_send_data(buffer);                                 \
+                                                                            \
+            memset(buffer, 0, size);                                        \
+            free(buffer);                                                   \
+        }                                                                   \
+    } while (0)
+
+#ifdef VERBUM_DEBUG_ENABLED
+    #define say_debug(FMT, ...)                                             \
+        do {                                                                \
+            lib_debug_print_internal(0, FMT, ##__VA_ARGS__);                \
+        } while (0)
+
+    #define say_debug_detail(FMT, ...)                                      \
+        do {                                                                \
+            lib_debug_print_internal(1, FMT, ##__VA_ARGS__);                \
+        } while (0)
+#else
+    #define say_debug(FMT, ...)         do { } while (0)
+    #define say_debug_detail(FMT, ...)  do { } while (0)
+#endif
+
+int verbum_debug_send_data (char *content);
+
+
+
+
+
 
 #define PACKAGE_NAME            "Verbum Network"
 #define PACKAGE_PREFIX_ARROW    " -> "
-
-/**
- * Internal macros.
- */
-
-// Debug macro.
-#define lib_debug_print_internal(DEBUG_FLAG, fmt, ...)                                  \
-    do {                                                                                \
-        time_t now     = time(NULL);                                                    \
-        struct tm *tms = localtime(&now);                                               \
-        char hour[5], min[5], sec[5];                                                   \
-                                                                                        \
-        memset(hour, 0x0, 5);                                                           \
-        memset(min,  0x0, 5);                                                           \
-        memset(sec,  0x0, 5);                                                           \
-                                                                                        \
-        sprintf(hour, "%d", tms->tm_hour);                                              \
-        sprintf(min,  "%d", tms->tm_min);                                               \
-        sprintf(sec,  "%d", tms->tm_sec);                                               \
-                                                                                        \
-        if (DEBUG_FLAG == 1)                                                            \
-            printf("[%s:%s:%s] -> %s:%d:%s(): " fmt "\n",                               \
-                hour, min, sec,                                                         \
-                __FILE__, __LINE__, __func__, ##__VA_ARGS__ );                          \
-        else                                                                            \
-            printf("[%s:%s:%s]: " fmt "\n",                                             \
-                hour, min, sec, ##__VA_ARGS__ );                                        \
-    } while (0)
-
-#define lib_debug_print(fmt, ...)                                                       \
-    do {                                                                                \
-        lib_debug_print_internal(1, fmt, ##__VA_ARGS__);                                \
-    } while(0)
-
-#define lib_say(fmt, ...)                                                               \
-    do {                                                                                \
-        lib_debug_print_internal(0, fmt, ##__VA_ARGS__);                                \
-    } while(0)
 
 /**
  * External macros.
@@ -49,31 +92,32 @@
 
 #define debug_print(fmt, ...)                                                           \
     do {                                                                                \
-        lib_debug_print(PACKAGE_NAME PACKAGE_PREFIX_ARROW fmt, ##__VA_ARGS__);          \
+        printf(fmt "\n", ##__VA_ARGS__);                                                \
     } while (0)
 
 #define say(fmt, ...)                                                                   \
     do {                                                                                \
-        lib_say(PACKAGE_NAME PACKAGE_PREFIX_ARROW fmt, ##__VA_ARGS__);                  \
+        printf(fmt "\n", ##__VA_ARGS__);                                                \
     } while(0)
 
 #define say_ret(RETURN, fmt, ...)                                                       \
     do {                                                                                \
-        lib_say(PACKAGE_NAME PACKAGE_PREFIX_ARROW fmt, ##__VA_ARGS__);                  \
+        printf(fmt "\n", ##__VA_ARGS__);                                                \
         return RETURN;                                                                  \
     } while(0)
 
 #define say_noret(fmt, ...)                                                             \
     do {                                                                                \
-        lib_say(PACKAGE_NAME PACKAGE_PREFIX_ARROW fmt, ##__VA_ARGS__);                  \
+        printf(fmt "\n", ##__VA_ARGS__);                                                \
         return;                                                                         \
     } while(0)
 
 #define say_exit(fmt, ...)                                                              \
     do {                                                                                \
-        lib_say(PACKAGE_NAME PACKAGE_PREFIX_ARROW fmt, ##__VA_ARGS__);                  \
+        printf(fmt "\n", ##__VA_ARGS__);                                                \
         exit(0);                                                                        \
     } while(0)
+
 
 /**
  * Macros with return value.
@@ -96,7 +140,8 @@
         debug_print(fmt, ##__VA_ARGS__);                                                \
         goto GOTO;                                                                      \
     } while (0)
-    
+
+
 #endif
 
 
