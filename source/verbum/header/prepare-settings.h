9e1124dcdf3dc1ab99693d6118349e90a31a3f7f
melchisedech333 <melchisedech333@gmail.com>
2022-09-27 17:27:15 -0300

    Update #0002, 27/09/2022 20:27:15 (UTC)

--- source/verbum/header/prepare-settings.h
+++ source/verbum/header/prepare-settings.h
@@ -1,8 +1,9 @@
 
 /**
- * Copyright (c) 2022, the Verbum project authors. Please see the AUTHORS file
- * for details. All rights reserved. Use of this source code is governed by a
- * BSD-style license that can be found in the LICENSE file.
+ * Copyright (c) 2022, the Verbum project authors. 
+ * 
+ * Please see the AUTHORS file for details. All rights reserved. Use of this source 
+ * code is governed by a BSD-style license that can be found in the LICENSE file.
  */
 
 #ifndef _H_VERBUM_PREPARE_SETTINGS_
