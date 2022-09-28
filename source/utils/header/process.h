4c7852af1714dd9eecb211a3c7364977f8f4ffa8
melchisedech333 <melchisedech333@gmail.com>
2022-09-27 23:21:54 -0300

    Update #0015

--- source/utils/header/process.h
+++ source/utils/header/process.h
@@ -1,11 +1,18 @@
 
-#ifndef VERBUM_LIBRARY_PROCESS
-#define VERBUM_LIBRARY_PROCESS
+/**
+ * Copyright (c) 2022, the Verbum project authors. 
+ * 
+ * Please see the AUTHORS file for details. All rights reserved. Use of this source 
+ * code is governed by a BSD-style license that can be found in the LICENSE file.
+ */
+
+#ifndef _H_UTILS_PROCESS_
+#define _H_UTILS_PROCESS_
 
 #include "global.h"
 
 pid_t check_process_running (char *name);
 
-#endif
+#endif /* _H_UTILS_PROCESS_ */
 
 
--- source/utils/header/types.h
+++ source/utils/header/types.h
@@ -1,4 +1,11 @@
 
+/**
+ * Copyright (c) 2022, the Verbum project authors. 
+ * 
+ * Please see the AUTHORS file for details. All rights reserved. Use of this source 
+ * code is governed by a BSD-style license that can be found in the LICENSE file.
+ */
+
 #ifndef _H_UTILS_TYPES_
 #define _H_UTILS_TYPES_
 
